5a7408acd1316ddc617e7a9d76ab92f7c6f1724d
seadev-io <admin@l.local>
2017-11-22 22:44:48 -1000

    2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-
-import * as $ from 'jquery'; 
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'app';
-
-  ngOnInit() {
-    $(document).ready(function() {
-      $("#signup").click(function() {
-        $.post( "/signup", $("#signupForm").serialize(),
-                function(data) {
-                  $("#signupSuccess").show();
-                }
-              )
-              .error(function(xhr) {
-                switch(xhr.status) {
-                  case 409:
-                    $("#signupDuplicate").show();
-                    break;
-                  default:
-                    $("#signupError").show();
-                }
-              })
-              .always(function() {
-                $("#signupModal").modal('hide');
-              });
-      })
-    })
-  }
 }
