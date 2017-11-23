7c45cd0ff04823fce2606cbab709f2f31d96c123
seadev-io <admin@l.local>
2017-11-22 22:34:42 -1000

    1

---
+++ server/app.ts
@@ -0,0 +1,94 @@
+// Include the cluster module
+import * as cluster from 'cluster';
+import * as AWS from 'aws-sdk';
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import * as path from 'path';
+
+// Code to run if we're in the master process
+if (cluster.isMaster) {
+
+    // Count the machine's CPUs
+    const cpuCount = require('os').cpus().length;
+
+    // Create a worker for each CPU
+    for (let i = 0; i < cpuCount; i += 1) {
+        cluster.fork();
+    }
+
+    // Listen for terminating workers
+    cluster.on('exit', function (worker) {
+
+        // Replace the terminated workers
+        console.log('Worker ' + worker.id + ' died :(');
+        cluster.fork();
+
+    });
+
+// Code to run if we're in a worker process
+} else {
+
+    AWS.config.region = process.env.REGION;
+
+    const sns = new AWS.SNS();
+    const ddb = new AWS.DynamoDB();
+
+    const ddbTable =  process.env.STARTUP_SIGNUP_TABLE;
+    const snsTopic =  process.env.NEW_SIGNUP_TOPIC;
+    const app = express();
+
+    app.use('/', express.static(path.join(__dirname, '../public')));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: false }));
+
+    app.get('/*', function(req, res) {
+        res.sendFile(path.join(__dirname, '../public/index.html'));
+      });
+
+    app.post('/signup', function(req, res) {
+        const item = {
+            'email': {'S': req.body.email},
+            'name': {'S': req.body.name},
+            'preview': {'S': req.body.previewAccess},
+            'theme': {'S': req.body.theme}
+        };
+
+        ddb.putItem({
+            'TableName': ddbTable,
+            'Item': item,
+            'Expected': { email: { Exists: false } }        
+        }, function(err, data) {
+            if (err) {
+                let returnStatus = 500;
+
+                if (err.code === 'ConditionalCheckFailedException') {
+                    returnStatus = 409;
+                }
+
+                res.status(returnStatus).end();
+                console.log('DDB Error: ' + err);
+            } else {
+                sns.publish({
+                    'Message': 'Name: ' + req.body.name + "\r\nEmail: " + req.body.email 
+                                        + "\r\nPreviewAccess: " + req.body.previewAccess 
+                                        + "\r\nTheme: " + req.body.theme,
+                    'Subject': 'New user sign up!!!',
+                    'TopicArn': snsTopic
+                }, function(err, data) {
+                    if (err) {
+                        res.status(500).end();
+                        console.log('SNS Error: ' + err);
+                    } else {
+                        res.status(201).end();
+                    }
+                });            
+            }
+        });
+    });
+
+    const port = process.env.PORT || 3000;
+
+    const server = app.listen(port, function () {
+        console.log('Server running at http://127.0.0.1:' + port + '/');
+    });
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import * as $ from 'jquery'; 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  ngOnInit() {
+    $(document).ready(function() {
+      $("#signup").click(function() {
+        $.post( "/signup", $("#signupForm").serialize(),
+                function(data) {
+                  $("#signupSuccess").show();
+                }
+              )
+              .error(function(xhr) {
+                switch(xhr.status) {
+                  case 409:
+                    $("#signupDuplicate").show();
+                    break;
+                  default:
+                    $("#signupError").show();
+                }
+              })
+              .always(function() {
+                $("#signupModal").modal('hide');
+              });
+      })
+    })
+  }
 }
